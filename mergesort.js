e827c3221789872acda26f1e91182b7472c956fb
Milo Calvo-Platero <macp92@gmail.com>
2019-10-22 17:02:16 -0400

    mergesorting complete

--- mergesort.js
+++ mergesort.js
@@ -10,29 +10,32 @@ function split(wholeArray) {
 
 function merge(array1, array2){
   let outputArray = []
-  //console.log(outputArray.length,array1.length,array2.length)
   let alength=array1.length+array2.length;
   while (outputArray.length < alength){
     let num;
-    
-    array1[0] < array2[0]? num = array1.shift() : num = array2.shift()
-    outputArray.push(num)
+    if (array1[0] < array2[0] || !array2[0]){
+      num = array1.shift()
+      outputArray.push(num)
+    }
+    else {
+      num = array2.shift();
+      outputArray.push(num)
+    }
   }
-
   return outputArray
 }
 
 function mergeSort(array) {
-  let result=[]
-  let count=array.length
-  while(count>1){
-    let oldarr1=mergeSort(split(array)[0])
-    console.log(oldarr1)
-    let oldarr2=mergeSort(split(array)[1])
-    // count/2
-    result=mergeSort(merge(...oldarr1,...oldarr2))
+  let result =[]
+  let count= array.length
+  if (count === 1){
+    return array
   }
-  console.log(result)
+  else if(count>1){
+    let oldarr1=mergeSort(split(array)[0])
+    let oldarr2=mergeSort(split(array)[1])
+    result = merge(oldarr1, oldarr2)
+  }
   return result
 
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -7,14 +7,15 @@ describe('Split Array function', function() {
 
 describe('Merge function', function(){
   it('is able to merge two sorted arrays into one sorted array', function(){
-    expect( merge([3, 5] ,[4, 7]) ).toEqual( [3, 4, 5, 7] );
+    expect( merge([3, 5] ,[4, 71]) ).toEqual( [3, 4, 5, 71] );
     // test the merging algorithm
   });
 });
 
 describe('MergeSort function', function(){
   it('is able to merge two sorted arrays into one sorted array', function(){
-    expect( mergeSort([3, 5,4, 7]) ).toEqual( [3, 4, 5, 7] );
+    expect( mergeSort([3, 5, 4, 7]) ).toEqual( [3, 4, 5, 7] );
+    expect( mergeSort([5, 18, 2, 6, 13, 20, 4]) ).toEqual( [2, 4, 5, 6, 13, 18, 20] )
     // test the merging algorithm
   });
 });
